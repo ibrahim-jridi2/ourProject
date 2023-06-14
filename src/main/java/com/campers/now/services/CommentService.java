81671bae2c543d691d37f4a17c90c94aa1f321b8
jnayeh <mouradslim44@gmail.com>
2023-06-14 12:28:45 +0100

    adds comment management

---
+++ src/main/java/com/campers/now/services/CommentService.java
@@ -0,0 +1,18 @@
+package com.campers.now.services;
+
+import com.campers.now.models.Comment;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+public interface CommentService {
+
+    List<Comment> getAll(Integer pageNumber, String property, Sort.Direction direction);
+
+    Comment getById(Integer id);
+    List<Comment> getByPostId(Integer id);
+
+    Comment add(Comment o);
+
+    Comment update(Comment o);
+}
