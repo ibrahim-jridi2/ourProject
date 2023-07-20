257f58e07074dc7c105453af10666b0cf91b2b98
BalssemZoghbi <zoghbibalssem@gmail.com>
2023-07-20 19:15:34 +0100

    fix errors

--- src/main/java/com/campers/now/repositories/ReservationRepository.java
+++ src/main/java/com/campers/now/repositories/ReservationRepository.java
@@ -2,8 +2,12 @@ package com.campers.now.repositories;
 
 import com.campers.now.models.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
-
+    @Query(value = "select * from reservation re where res.user_id=:userId",nativeQuery = true)
+    List<Reservation> findReservationByUserId(Integer userId);
 
 }
--- src/main/java/com/campers/now/services/Impl/ReservationServiceImpl.java
+++ src/main/java/com/campers/now/services/Impl/ReservationServiceImpl.java
@@ -1,33 +1,30 @@
 package com.campers.now.services.Impl;
 
 import com.campers.now.models.Reservation;
+import com.campers.now.models.User;
 import com.campers.now.repositories.ReservationRepository;
+import com.campers.now.repositories.UserRepository;
 import com.campers.now.services.ReservationService;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
 import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.Date;
 import java.util.List;
+
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class ReservationServiceImpl implements ReservationService {
     ReservationRepository reservationRepository;
+    UserRepository userRepository;
 
     @Override
-    public List<Reservation> getAll(Integer pageNumber, String property, Sort.Direction direction) {
-        if (pageNumber == null)
+    public List<Reservation> getAll() {
             return reservationRepository.findAll();
-        return reservationRepository.findAll(PageRequest.of((pageNumber <= 0 ? 1 : pageNumber) - 1, 10, Sort.by(List.of(Sort.Order.by(StringUtils.hasText(property) ? property : "id").with(direction)))))
-                .stream().collect(Collectors.toUnmodifiableList());
     }
 
     @Override
@@ -73,5 +70,8 @@ public class ReservationServiceImpl implements ReservationService {
 
         return totalDays;
     }
+
+
+
 
 }
