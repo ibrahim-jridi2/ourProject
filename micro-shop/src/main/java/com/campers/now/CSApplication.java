567c32b8a21818a20e5ac4bab4fee229cf9b86aa
amani nasri <a.nasri@numeryx.fr>
2024-04-01 22:44:22 +0100

    eureka client and connect with gateway

--- micro-shop/src/main/java/com/campers/now/CSApplication.java
+++ micro-shop/src/main/java/com/campers/now/CSApplication.java
@@ -3,9 +3,12 @@ package com.campers.now;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableEurekaClient
+
 public class CSApplication {
 
     public static void main(String[] args) {
