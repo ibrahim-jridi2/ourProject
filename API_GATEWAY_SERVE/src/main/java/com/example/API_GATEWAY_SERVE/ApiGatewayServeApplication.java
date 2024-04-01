886f928bb1b7746cf48fe8f6d249a3f64f4b8cbd
jridi ibrahim <ibrahim.jridi@esprit.tn>
2024-04-01 20:08:40 +0100

    modifing gateway pom.xml

--- API_GATEWAY_SERVE/src/main/java/com/example/API_GATEWAY_SERVE/ApiGatewayServeApplication.java
+++ API_GATEWAY_SERVE/src/main/java/com/example/API_GATEWAY_SERVE/ApiGatewayServeApplication.java
@@ -2,6 +2,7 @@ package com.example.API_GATEWAY_SERVE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
