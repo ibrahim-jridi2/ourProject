6014d2e62a191eeae0a890519227d956521d1679
ayed <78506293+ayed-hachich@users.noreply.github.com>
2024-04-08 16:51:39 +0100

    add reservation img + gateway

--- reservation_Micro/src/main/java/com/campers/now/CSApplication.java
+++ reservation_Micro/src/main/java/com/campers/now/CSApplication.java
@@ -6,6 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableEurekaClient
 public class CSApplication {
 
     public static void main(String[] args) {
